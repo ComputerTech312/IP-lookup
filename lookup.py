f37c9c588035ee5868b63c09437f6f75cbde05ad
Colby <60820431+ComputerTech312@users.noreply.github.com>
2022-05-06 03:42:54 +0100

    update lookup.py

--- lookup.py
+++ lookup.py
@@ -1,1 +1,13 @@
+import requests
+import json
 
+while True:
+    print('Enter the IP to lookup')
+    ip = input('IP: ')
+    break
+
+url = 'http://ipwho.is/' + ip
+response = requests.get(url)
+data = json.loads(response.text)
+
+print(data)
